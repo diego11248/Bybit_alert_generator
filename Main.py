08c72fd48ecfeaa5b83e476e06db56b7eac93dda
diego11248 <127966544+diego11248@users.noreply.github.com>
2026-01-28 18:42:53 -0500

    Implement valid_time method for trading hours validation
    Added valid_time method to check market hours and manage trading.

--- Main.py
+++ Main.py
@@ -27,4 +27,60 @@ class Main:
         self.mode = 1  # 1 - Isolated, 0 - Cross
         self.leverage = 1
         self.day = 0;
+
+    def valid_time(self, open_positions):
+        curr = datetime.datetime.now(timezone.utc) # Use UTC instead of local time
+        hour = curr.hour 
+        minute = curr.minute
+        self.day = curr.weekday()
+        
+        print(f"Current UTC hour: {curr}", flush=True)
+
+        if(self.day == 5 or self.day == 6):
+            #self.close_all(open_positions)
+            print("Its the weekend")
+            return False
+            
+
+        if hour > 21 or hour <= 14:                #edge hours for trading the last hour of trading, the first hour of trading
+
+            if( hour == 0 and minute > 1 ):
+                self.close_all(open_positions)
+                print("Sleeping... Market is not open.")
+                sleep(180)
+                return False
+            
+            elif (hour > 21) or (hour < 14):
+                #self.close_all(open_positions)
+                print("Sleeping... Market is not open.")
+                sleep(180)
+                return False
+                
+            elif hour == 21 and minute <= 25:
+                #self.close_all(open_positions)
+                print("Sleeping... Market is not open.")
+                sleep(180)
+                return False
+
+            
+
+        if(hour >= 17 and hour < 19):                        #laggy time for trading (closed hours for the strategy)
+            if (hour == 17 and minute >= 30):
+                #self.close_all(open_positions)
+                print("Sleeping... its lunch time")
+                sleep(180)
+                return False
+            elif(hour > 17 and  hour < 19):
+                #self.close_all(open_positions)
+                print("Sleeping... its lunch time")
+                sleep(180)
+                return False
+                
+           
+                
+              
+        else:
+            print("Market is open, running bot")
+            return True
+
         
