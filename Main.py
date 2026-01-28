ff40b84d8f1af5f1510f508f8f93fcae7eb1d04a
diego11248 <127966544+diego11248@users.noreply.github.com>
2026-01-28 18:40:52 -0500

    Remove trading configuration parameters
    Removed various configuration parameters related to trading strategy.

--- Main.py
+++ Main.py
@@ -24,19 +24,7 @@ class Main:
         self.strategy = Derivatives()  
 
         # Configurations
-        self.tp = 0.03 # Take profit 3.5%
-        self.sl = 0.09  # Stop loss 2.5%
-        self.sl_multiplier = 2.0 #multiplier for ATR
-        self.tp_multiplier =  6 #multiplier for ATR
-        self.trail_active = 2.4 #activation multiplier for trailing
-        self.trail_dist = 1.2 #distance multipplier for trailing
         self.mode = 1  # 1 - Isolated, 0 - Cross
         self.leverage = 1
-        self.qty = 5  # Quantity of USDT to be traded
-        self.risk_percentage = 0.03
-        self.max_positions =  min(math.floor(1/self.risk_percentage), 25)  # Maximum open trades
-        self.percentual_gain = []
-        self.optimal_market = True
-        self.losing_streak = 0
         self.day = 0;
         
